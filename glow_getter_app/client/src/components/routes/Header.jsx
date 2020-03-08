66cf3eb4021eff3255bfc390d84effd459d226eb
swr127 <samantha.w.russell@gmail.com>
2020-03-08 15:48:51 -0400

    created css files

---
+++ glow_getter_app/client/src/components/routes/Header.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+// Change to class component for React Hamburger Menu
+function Header() {
+    return (
+        <div className="Header">
+            Header
+        </div>
+    )
+}
+
+export default Header
---
+++ glow_getter_app/client/src/components/routes/Login.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+// Change to class component for Logn User Auth
+function Login() {
+    return (
+        <div className="Login">
+            Login
+        </div>
+    )
+}
+
+export default Login
