345bc24608f457f1385093fa875eef191e3b820d
swr127 <samantha.w.russell@gmail.com>
2020-03-11 18:45:47 -0400

    flip animation working

--- glow_getter_app/client/src/components/routes/Cart.jsx
+++ glow_getter_app/client/src/components/routes/Cart.jsx
@@ -23,19 +23,40 @@ class Cart extends Component {
 
     render() {
         const products = this.state.products.map(product => (
-            <div key={product.id}>
-                <img src={product.img} width='200px' height='200px' alt="product" /> <br />
-                <h4>{product.name}</h4> <br/>
-                <h5>{product.price}</h5> <br />
-                <button name={product.id} onClick={this.props.handleCart}>Remove from Cart</button>
+          <div className="Product-Card" key={product.id}>
+            <div className="Product-Image">
+                <div className="Product-Front"><img src={product.img} width='200px' height='200px' alt="product" /></div>
+                <div className="Product-Back"><img src="https://i.imgur.com/xG0rVGt.jpg" width='200px' height='200px' alt="product" /></div>
             </div>
+                
+            <h3 className="Product-Name">{product.name}</h3>
+            <h4 className="Product-Price">${product.price}</h4>
+            <button name={product.id} onClick={this.props.handleCart}>Remove from Cart</button>
+            
+          </div>
         ))
         return (
-          <div className="Shop">
-            { products.length > 0 ?
-              <div> {products} </div> :
-              <div>You're cart is empty!</div>
-            }
+          <div className="Cart">
+            <div className="Cart-Instructions">
+              <h1>Your Cart!</h1>
+              <h4>Place instructions here</h4>
+              <ul>
+                <li>Step One: text text text</li>
+                <li>Step Two: text text text</li>
+                <li>Step Three: text text text</li>
+                <li>Step Four: text text text</li>
+                <li>Step Five: text text text</li>
+                <li>Step Six: text text text</li>
+              </ul>
+              <img src="https://i.imgur.com/beGText.jpg" width='550px' height='350px' alt="girls" /> <br />
+              <p>#glowgetters via Instagram</p>
+            </div>
+            <div className="Cart-Box">
+              { products.length > 0 ?
+                <div className="Cart-Products"> {products} </div> :
+                <h1 className="Cart-Empty">>You're cart is empty!</h1>
+              }
+            </div>
           </div>
         )
     }
