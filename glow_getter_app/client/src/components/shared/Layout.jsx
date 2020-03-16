37f9f574d710ba001d6a9f709ebe5f92f22c74c5
swr127 <samantha.w.russell@gmail.com>
2020-03-16 12:57:05 -0400

    polished footer

--- glow_getter_app/client/src/components/shared/Layout.jsx
+++ glow_getter_app/client/src/components/shared/Layout.jsx
@@ -81,9 +81,11 @@ class Layout extends Component {
 
             <div className="Footer">
                 <hr />
-                <p>&copy; 2020 SAMANTHA RUSSELL  |  
-                    <a href="https://github.com/swr127"><img src="https://i.imgur.com/5KK8yEl.png" width="30px" height="30px" /></a>            
-                    <a href="https://www.linkedin.com/in/swr127/"><img src="https://i.imgur.com/Wrxz7wH.png" width="30px" height="30px" /></a>
+                <p>
+                    <a href="https://github.com/swr127"><img src="https://i.imgur.com/5KK8yEl.png" width="40px" height="40px" /></a>            
+                    <a href="https://www.linkedin.com/in/swr127/"><img src="https://i.imgur.com/Wrxz7wH.png" width="40px" height="40px" /></a>
+                    <br />
+                    &copy; 2020 SAMANTHA RUSSELL
                 </p>
             </div>
         </div>
