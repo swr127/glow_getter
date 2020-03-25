e358d4eb94c98a44b5bf8bf07a7e0dba2556e1ba
swr127 <samantha.w.russell@gmail.com>
2020-03-25 13:15:23 -0400

    ready for deployment

--- glow_getter_app/config/routes.rb
+++ glow_getter_app/config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   get '/cart' => 'users#cart'
   post '/cart/:product_id' => 'users#cartadd'
   delete '/cart/:product_id' => 'users#cartremove'
-  
+
   resources :users
   resources :products
-  
+
+  get '*path', to: 'application#fallback_index_html', constraints: lambda { |request|
+    !request.xhr? && request.format.html?
+  }
 end
